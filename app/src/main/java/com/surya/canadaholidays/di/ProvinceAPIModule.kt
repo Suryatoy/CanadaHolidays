997904b87731a389bfb96a18306302d9e0df1414
Surya <Surya.friendz@gmail.com>
2020-07-08 20:39:57 -0400

    Variable naming convention

--- app/src/main/java/com/surya/canadaholidays/di/ProvinceAPIModule.kt
+++ app/src/main/java/com/surya/canadaholidays/di/ProvinceAPIModule.kt
@@ -9,14 +9,14 @@ import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 
 @Module
-class ProvinceAPIModule {
+open class ProvinceAPIModule {
 
-    private val BASE_URL = "https://canada-holidays.ca/api/v1/"
+    private val mBaseUrl = "https://canada-holidays.ca/api/v1/"
 
     @Provides
     fun provideProvinceAPI(): ProvinceAPI {
         return Retrofit.Builder()
-            .baseUrl(BASE_URL)
+            .baseUrl(mBaseUrl)
             .addConverterFactory(GsonConverterFactory.create())
             .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
             .build()
