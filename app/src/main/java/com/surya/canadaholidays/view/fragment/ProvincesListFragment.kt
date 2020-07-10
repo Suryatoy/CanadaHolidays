bc52e0277914d920274d990e75eee0485df1b8c0
Surya <Surya.friendz@gmail.com>
2020-07-09 22:26:14 -0400

    Information view added

--- app/src/main/java/com/surya/canadaholidays/view/fragment/ProvincesListFragment.kt
+++ app/src/main/java/com/surya/canadaholidays/view/fragment/ProvincesListFragment.kt
@@ -2,20 +2,25 @@ package com.surya.canadaholidays.view.fragment
 
 import android.graphics.Canvas
 import android.graphics.Color
+import android.graphics.Color.blue
+import android.graphics.Color.red
+import android.graphics.Typeface
 import android.graphics.drawable.ColorDrawable
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.animation.AnimationUtils
 import androidx.annotation.NonNull
-import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.getkeepsafe.taptargetview.TapTarget
+import com.getkeepsafe.taptargetview.TapTargetView
 import com.surya.canadaholidays.R
 import com.surya.canadaholidays.model.Province
 import com.surya.canadaholidays.util.Constants
@@ -42,6 +47,7 @@ class ProvincesListFragment : Fragment() {
             provincesRecyclerView.visibility = View.VISIBLE
             listAdapter.updateProvincesList(it)
             provinceList = it
+            showInfo()
         }
     }
 
@@ -102,6 +108,40 @@ class ProvincesListFragment : Fragment() {
         }
     }
 
+    private fun showInfo() {
+        TapTargetView.showFor(activity,  // `this` is an Activity
+            TapTarget.forView(
+                refreshLayout,
+                getString(R.string.fav_province),
+                getString(R.string.swipe_recyclerview)
+            ) // All options below are optional
+                // .outerCircleColor(R.color.red) // Specify a color for the outer circle
+                .outerCircleAlpha(0.96f) // Specify the alpha amount for the outer circle
+                .targetCircleColor(R.color.white) // Specify a color for the target circle
+                .titleTextSize(20) // Specify the size (in sp) of the title text
+                .titleTextColor(R.color.white) // Specify the color of the title text
+                .descriptionTextSize(15) // Specify the size (in sp) of the description text
+                //   .descriptionTextColor(R.color.red) // Specify the color of the description text
+                //   .textColor(R.color.blue) // Specify a color for both the title and description text
+                .textTypeface(Typeface.SANS_SERIF) // Specify a typeface for the text
+                //   .dimColor(R.color.black) // If set, will dim behind the view with 30% opacity of the given color
+                .drawShadow(true) // Whether to draw a drop shadow or not
+                .cancelable(true) // Whether tapping outside the outer circle dismisses the view
+                .tintTarget(true) // Whether to tint the target view's color
+                .transparentTarget(false) // Specify whether the target is transparent (displays the content underneath)
+                //.icon(R.drawable.) // Specify a custom drawable to draw as the target
+                .targetRadius(60),  // Specify the target radius (in dp)
+            object : TapTargetView.Listener() {
+                // The listener can listen for regular clicks, long clicks or cancels
+                override fun onTargetClick(view: TapTargetView) {
+                    view.dismiss(true)
+                }
+            })
+
+
+    }
+
+
     /**
      * To animate provinces recyclerview when data is refreshed
      */
@@ -128,8 +168,8 @@ class ProvincesListFragment : Fragment() {
             }
 
             override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
-               val provinceId = provinceList[viewHolder.adapterPosition].id
-                addPreference(Constants.PROVINCE_ID,provinceId)
+                val provinceId = provinceList[viewHolder.adapterPosition].id
+                addPreference(Constants.PROVINCE_ID, provinceId)
                 listAdapter.notifyDataSetChanged()
                 animateProvincesList()
             }
